997a71c1715be6ea44b89cddb71298e336dfb2d8
Abel <abel0130@163.com>
2021-11-23 21:36:41 +0800

    springsecurity

--- springSecurity/src/main/java/me/abel/security/SecurityApplication.java
+++ springSecurity/src/main/java/me/abel/security/SecurityApplication.java
@@ -2,12 +2,14 @@ package me.abel.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * @Description
  * @Author Abel.li
  * @Date 2021/10/25 下午18:35
  */
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication
 public class SecurityApplication {
 
--- springSecurity/src/main/java/me/abel/security/config/WebSecurityDbBaseConfig.java
+++ springSecurity/src/main/java/me/abel/security/config/WebSecurityDbBaseConfig.java
@@ -1,14 +1,21 @@
 package me.abel.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
 
 /**
  * @Description
@@ -21,11 +28,22 @@ public class WebSecurityDbBaseConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Bean
     PasswordEncoder passwordEncoder(){
 //        return NoOpPasswordEncoder.getInstance();
 //        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // create default
         return new BCryptPasswordEncoder(); // default
+    }
+
+    @Bean
+    PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        repository.setJdbcTemplate(jdbcTemplate);
+        repository.setCreateTableOnStartup(true);
+        return repository;
     }
 
     @Override
@@ -35,14 +53,19 @@ public class WebSecurityDbBaseConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity hs) throws Exception {
+        hs.exceptionHandling().accessDeniedPage("/noauth");
+        hs.logout()  //开启注销登陆
+            .logoutUrl("/logout")  //注销登陆请求url
+            .logoutSuccessUrl("/login")
+                .permitAll()
+            .clearAuthentication(true)  //清除身份信息
+            .invalidateHttpSession(true);  //session失效
         hs.formLogin()
                 .loginPage("index.html")
                 .loginProcessingUrl("login")
                 .defaultSuccessUrl("index").permitAll()
-                .and().authorizeRequests()
-                .antMatchers("/", "/access").permitAll()
-                .antMatchers("/auth").hasAuthority("admin")
-                .anyRequest().authenticated()
+                .and().authorizeRequests().antMatchers("/", "/access").permitAll().antMatchers("/auth").hasAuthority("admin").anyRequest().authenticated()
+                .and().rememberMe().tokenRepository(persistentTokenRepository()).rememberMeParameter("remember").tokenValiditySeconds(10).userDetailsService(userDetailsService)
                 .and().csrf().disable(); //关闭CSRF防护
     }
 }
