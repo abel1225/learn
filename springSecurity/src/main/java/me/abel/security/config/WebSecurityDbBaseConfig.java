ec05e7edecd75af885e6b9f1a461b9027a0f2e37
Abel <abel0130@163.com>
2021-11-23 20:09:50 +0800

    springsecurity

--- springSecurity/src/main/java/me/abel/security/config/WebSecurityDbBaseConfig.java
+++ springSecurity/src/main/java/me/abel/security/config/WebSecurityDbBaseConfig.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,11 +25,25 @@ public class WebSecurityDbBaseConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+//        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity hs) throws Exception {
+        hs.formLogin()
+                .loginPage("index.html")
+                .loginProcessingUrl("login")
+                .defaultSuccessUrl("index").permitAll()
+                .and().authorizeRequests()
+                .antMatchers("/", "/access").permitAll()
+                .antMatchers("/auth").hasAuthority("admin")
+                .anyRequest().authenticated()
+                .and().csrf().disable(); //关闭CSRF防护
     }
 }
