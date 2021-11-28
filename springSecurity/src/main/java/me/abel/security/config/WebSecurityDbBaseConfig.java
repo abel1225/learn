928e2937dd31dbc4df93eb77e9b40afa9428ba36
Abel <abel0130@163.com>
2021-11-28 21:35:50 +0800

    springsecurity

--- springSecurity/src/main/java/me/abel/security/config/WebSecurityDbBaseConfig.java
+++ springSecurity/src/main/java/me/abel/security/config/WebSecurityDbBaseConfig.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -53,17 +54,27 @@ public class WebSecurityDbBaseConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity hs) throws Exception {
+        /**
+         * 设置无权限跳转
+         */
         hs.exceptionHandling().accessDeniedPage("/noauth");
+        /**
+         * 设置session策略
+         */
+        hs.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
         hs.logout()  //开启注销登陆
             .logoutUrl("/logout")  //注销登陆请求url
             .logoutSuccessUrl("/login")
+//                .invalidateHttpSession(true)
                 .permitAll()
             .clearAuthentication(true)  //清除身份信息
             .invalidateHttpSession(true);  //session失效
         hs.formLogin()
                 .loginPage("index.html")
                 .loginProcessingUrl("login")
-                .defaultSuccessUrl("index").permitAll()
+                .defaultSuccessUrl("index")
+                .permitAll()
                 // auth
                 .and().authorizeRequests().antMatchers("/", "/access").permitAll().antMatchers("/auth").hasAuthority("admin").anyRequest().authenticated()
                 // remember me
